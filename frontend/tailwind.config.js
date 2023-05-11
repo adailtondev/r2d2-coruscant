af57ace759a2df25e8e11bf26a2009e6397ec3f0
adailtondev <adailtonh.dev@gmail.com>
2023-05-10 22:04:08 -0300

    Iniciação do front

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    'bg-red-600',
+    'bg-blue-600',
+    'bg-green-600',
+    'hover:bg-amber-100',
+    'bg-amber-400',
+    'hover:bg-lime-100',
+    'bg-lime-400',
+    'hover:bg-teal-100',
+    'bg-teal-400',
+    'hover:bg-indigo-100',
+    'bg-indigo-400'
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
