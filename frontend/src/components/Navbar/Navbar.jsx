599e7a63d6046923a67acff51bdc46a7e38cc592
adailtondev <adailtonh.dev@gmail.com>
2023-05-16 14:48:13 -0300

    correção logo

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const navigate = useNavigate()
   return (
     <header className="flex navbar justify-between w-screen">
-      <img src="../../src/assets/logo.png" alt="" className='w-[10%] ml-16'/>
+      <img src="https://assets.stickpng.com/thumbs/602bb8655c5b2300043add95.png" alt="" className='w-[10%] ml-16'/>
       <ul className="flex gap-14 items-center justify-center mr-28">
         <li className="hover:bg-cyan-700 px-16 py-4 rounded-md group ease-in duration-300 cursor-pointer" >
           <a href="#" className="text-white group-hover:text-gray-100">
