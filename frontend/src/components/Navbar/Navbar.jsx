625db19a5db8b39948e1f9c97f987a1ad8f783cc
adailtondev <adailtonh.dev@gmail.com>
2023-06-02 22:48:46 -0300

    tentativa de consertar bug

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -8,12 +8,12 @@ const Navbar = () => {
       <img src="https://en.logodownload.org/wp-content/uploads/2020/09/star-wars-logo-3-11-1024x443.png" alt="" className='w-[8%] h-[3%] mt-2 ml-16'/>
       <ul className="flex gap-14 items-center justify-center mr-28">
         <li className="hover:bg-cyan-700 px-16 py-4 rounded-md group ease-in duration-300 cursor-pointer" >
-          <a href="" className="text-white group-hover:text-gray-100" onClick={()=> navigate('/')}>
+          <a href={navigate('/')} className="text-white group-hover:text-gray-100" onClick={()=> navigate('/')}>
             Ínicio
           </a>
         </li>
         <li className="hover:bg-cyan-700 px-16 py-4 rounded-md group ease-in duration-300 cursor-pointer" onClick={()=> navigate('/movies')}>
-          <a className="text-white group-hover:text-gray-100" href="" >
+          <a className="text-white group-hover:text-gray-100" href={navigate('/movies')} >
             Filmes
           </a>
         </li>
